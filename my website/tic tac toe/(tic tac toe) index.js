cb6068f322607a32a6432f598a802e9171570aea
it'smepressly <130832755+itspressly@users.noreply.github.com>
2023-06-26 14:26:00 -0700

    first commit

--- my website/tic tac toe/(tic tac toe) index.js
+++ my website/tic tac toe/(tic tac toe) index.js
@@ -8,15 +8,17 @@ const possiblechoices = document.querySelectorAll('button')
 
 let playerchoice
 let computerchoice
+let result
 
 possiblechoices.forEach(possiblechoice => possiblechoice.addEventListener('click', (e) => {
 playerchoice = e.target.id
 playerchoicedisplay.innerHTML = playerchoice
 generatecomputerchoice()
+getresult()
 }))
 
 function generatecomputerchoice() {
-    const randomnumber = math.floor(math.random()  * 3) // or you can use possiblechoices.length 
+    let randomnumber = Math.floor(Math.random()  * 3 + 1) // or you can use possiblechoices.length 
    
 
     if (randomnumber === 1) {
@@ -30,6 +32,41 @@ function generatecomputerchoice() {
     if (randomnumber === 3) {
         computerchoice = 'paper'
     }
+
+
+    computerchoicedisplay.innerHTML = computerchoice
 }
 
-computerchoicedisplay.innerHTML = computerchoice
+function getresult() {
+    if (computerchoice === playerchoice) {
+        result = 'its a draw!'
+    }
+
+    if (computerchoice ===  'rock'&& playerchoice === "paper") {
+        result = 'you win!'
+    }
+
+    if (computerchoice ===  'rock'&& playerchoice === "scissors") {
+        result = 'you lost!'
+    }
+
+    if (computerchoice ===  'paper'&& playerchoice === "scissors") {
+        result = 'you win!'
+    }
+
+    if (computerchoice ===  'paper'&& playerchoice === "rock") {
+        result = 'you lost!'
+    }
+    if (computerchoice ===  'scissors'&& playerchoice === "rock") {
+        result = 'you win!'
+    }
+
+    if (computerchoice ===  'scissors'&& playerchoice === "paper") {
+        result = 'you lost!'
+    }
+
+    resultdisplay.innerHTML = result
+}
+
+
+
